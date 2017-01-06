987b90c2a2e94814a1bdf82cba9fe390bcfeb3ce
Uyttersprot Pieter <Pieter.u@hotmail.com>
2017-01-06 17:51:21 +0100

    Configured Database Provider

---
+++ src/SportsStorePieter/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStorePieter.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring = @"Server=.\SQLEXPRESS;Database=SportsStorePieter;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
